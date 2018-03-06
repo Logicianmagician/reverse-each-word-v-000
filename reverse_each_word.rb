d2ea70abd0d9de4bc42a681767fe18c6c8adf464
Andrew Kairys <andrewkairys@gmail.com>
2018-03-06 20:30:08 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(word)
  word.reverse.split.reverse.join(" ")
 end
 
-def reverse_each_word_collect(sentence)
+def reverse_collect(sentence)
   sentence.split.collect {|word| word.reverse}.join(" ")
 end
